ae0d5191e943bcddb3df45aae2cd55bbf521ed59
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-06-21 17:58:51 -0300

    refactored the logic

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,15 +5,18 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    @question = params[:questions]
-      if @question.blank?
+
+    @question = params[:question]
+    if  @question == 'I am going to work'
+      @answer = 'Great!'
+    elsif @question.end_with? ("?")
+      @answer = 'Silly question, get dressed and go to work!.'
+    elsif @answer.blank?
         @answer = "I can't hear you!"
-        elsif @question = "I am going to work"
-          @answer = "Great!"
-        elsif @question.ends_with?("?")
-      @answer = "Silly question, get dressed and go to work!"
-        else
-          @answer = "I don't care, get dressed and go to work!"
-      end
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
+
+
