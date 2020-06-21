ee719df486e3e4bb00c6aae23eb59a423ce7b1fc
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-06-21 16:39:50 -0300

    created the logic for answer method

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,9 +4,16 @@ class QuestionsController < ApplicationController
 
   end
 
-
   def answer
-
+    @question = params[:questions]
+      if @question.blank?
+        @answer = "I can't hear you!"
+        elsif @question = "I am going to work"
+          @answer = "Great!"
+        elsif @question.ends_with?("?")
+          @answer = 'Silly question, get dressed and go to work!'
+        else
+          @answer = "I don't care, get dressed and go to work!"
+      end
   end
-
 end
